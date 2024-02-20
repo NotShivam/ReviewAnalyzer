a94a6762f4f158c177eafd1e2adfc83088c0ea4e
Shivam Trivedi <strivedi947@gmail.com>
2024-02-20 21:58:06 +0530

    first commit

---
+++ src/variables/sampleData.js
@@ -0,0 +1,149 @@
+export const apiData = {
+    "prodName": "Redmi 12 5G Moonstone Silver 6GB RAM 128GB ROM",
+    "totRev": "10,235",
+    "adjRev": "6,513",
+    "orgRat": "4.0",
+    "adjRat": "4.0",
+    "fakeReviews": "36.37%",
+    "img": "https://m.media-amazon.com/images/I/41-2m5Oyv5L.jpg",
+    "amazon": {
+      "price": "12,999",
+      "url": "https://www.amazon.in/Redmi-Moonstone-Silver-4GB-128GB/dp/B0C9J97Z2D/ref=lp_1389401031_1_1?sbo=RZvfv%2F%2FHxDF%2BO5021pAnSA%3D%3D",
+      "rating": {
+        "one": "12%",
+        "two": "4%",
+        "three": "9%",
+        "four": "25%",
+        "five": "50%"
+      }
+    },
+    "trusted": {
+      "title": "Most Trusted Reviews",
+      "reviews": [
+        {
+          "rating": "3/5",
+          "review": "Its been almost a month i bought this phone and ...",
+          "showMore": "https://www.amazon.in/gp/customer-reviews/R4XZ36OLCEEXV?tag=reviewmeta0b-20"
+        },
+        {
+          "rating": "4/5",
+          "review": "Writing the review using the same phone. So it is ...",
+          "showMore": "https://www.amazon.in/gp/customer-reviews/R4XZ36OLCEEXV?tag=reviewmeta0b-20"
+        },
+        {
+          "rating": "4/5",
+          "review": "Review after 1 monthsLooks - 10/10Battery Life- 7/10Network Reception- 10/10Charging ...",
+          "showMore": "https://reviewmeta.com/profile/amazon-in/AE5W5ZXZ62T2XEJCFIMPXAHRZQFA?tag=reviewmeta0b-20"
+        },
+        {
+          "rating": "4/5",
+          "review": "Ok",
+          "showMore": "https://www.amazon.in/gp/customer-reviews/R3UIR8QDJHDBEY?tag=reviewmeta0b-20"
+        },
+        {
+          "rating": "1/5",
+          "review": "User did not leave written text for this review.",
+          "showMore": "https://www.amazon.in/gp/customer-reviews/R3UIR8QDJHDBEY?tag=reviewmeta0b-20"
+        }
+      ]
+    },
+    "leastTrusted": {
+      "title": "Least Trusted Reviews",
+      "reviews": [
+        {
+          "rating": "1/5",
+          "review": "Unhappy with quality. Sounds and camera not at all good. ...",
+          "showMore": "https://www.amazon.in/gp/customer-reviews/R33YRBNWILJFK3?tag=reviewmeta0b-20"
+        },
+        {
+          "rating": "4/5",
+          "review": "Paid 15499 and worth price. We are using from last ...",
+          "showMore": "https://www.amazon.in/gp/customer-reviews/R33YRBNWILJFK3?tag=reviewmeta0b-20"
+        },
+        {
+          "rating": "1/5",
+          "review": "Not a gaming phone daily usage phone bgmi gyro sensitivity ...",
+          "showMore": "https://reviewmeta.com/profile/amazon-in/AH4UBUIPTTMHMSF463TEQCUDMA6Q?tag=reviewmeta0b-20"
+        },
+        {
+          "rating": "3/5",
+          "review": "Charging is very slow and bettery low very fast . ...",
+          "showMore": "https://www.amazon.in/gp/customer-reviews/R25HUA83FJ0X1D?tag=reviewmeta0b-20"
+        },
+        {
+          "rating": "1/5",
+          "review": "Feature vise phone thik h but battery bekar aati hai ...",
+          "showMore": "https://www.amazon.in/gp/customer-reviews/R25HUA83FJ0X1D?tag=reviewmeta0b-20"
+        }
+      ]
+    },
+    "unveriPurchase": {
+      "title": "Unverified Purchases",
+      "perc": "2%",
+      "num": "194",
+      "ratings": [
+        "1.5/5",
+        "3.6/5"
+      ]
+    },
+    "sus": {
+      "title": "Suspicious Reviewers",
+      "perc": "~50%",
+      "ratings": [
+        "3.6/5",
+        "3.2/5"
+      ]
+    },
+    "trend": {
+      "title": "Rating Trend",
+      "perc": "~20%"
+    },
+    "phaseRep": {
+      "title": "Phrase Repetition",
+      "perc": "~70%",
+      "ratings": [
+        "3.4/5",
+        "3.3/5"
+      ],
+      "inReview": [
+        {
+          "text": "could be better",
+          "perc": "~14% of reviews"
+        },
+        {
+          "text": "but it could be better",
+          "perc": "~9% of reviews"
+        }
+      ],
+      "inTitles": [
+        {
+          "text": "4.0 out of 5 stars",
+          "perc": "~50% of reviews"
+        },
+        {
+          "text": "1.0 out of 5 stars",
+          "perc": "~18% of reviews"
+        },
+        {
+          "text": "5.0 out of 5 stars",
+          "perc": "~14% of reviews"
+        }
+      ]
+    },
+    "wordCount": {
+      "title": "Overrepresented Word Count",
+      "perc": "<10%"
+    },
+    "overl": {
+      "title": "Overlapping Review History",
+      "perc": "<10%",
+      "ratings": [
+        "4.0/5",
+        "3.3/5"
+      ]
+    },
+    "deleted": {
+      "title": "Deleted Reviews",
+      "num": "0"
+    }
+  }
---
+++ src/views/admin/default/index.jsx
@@ -0,0 +1,105 @@
+import MiniCalendar from "components/calendar/MiniCalendar";
+import WeeklyRevenue from "views/admin/default/components/WeeklyRevenue";
+import TotalSpent from "views/admin/default/components/TotalSpent";
+import PieChartCard from "views/admin/default/components/PieChartCard";
+import { IoMdHome } from "react-icons/io";
+import { IoDocuments } from "react-icons/io5";
+import { MdBarChart, MdDashboard } from "react-icons/md";
+
+import { columnsDataCheck, columnsDataComplex, complexReview } from "./variables/columnsData";
+
+import Widget from "components/widget/Widget";
+import CheckTable from "views/admin/default/components/CheckTable";
+import ComplexTable from "views/admin/default/components/ComplexTable";
+import DailyTraffic from "views/admin/default/components/DailyTraffic";
+import TaskCard from "views/admin/default/components/TaskCard";
+import tableDataCheck from "./variables/customData.json";
+import tableDataComplex from "./variables/tableDataComplex.json";
+import {apiData} from '../../../variables/sampleData.js'
+
+const Dashboard = () => {
+  return (
+    <div>
+      {/* Card widget */}
+
+      <div className="mt-3 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-3 3xl:grid-cols-6">
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={"Total Reviews"}
+          subtitle={apiData.totRev}
+        />
+        <Widget
+          icon={<IoDocuments className="h-6 w-6" />}
+          title={"Product Rating"}
+          subtitle={apiData.orgRat}
+        />
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={"Today Sales"}
+          subtitle={"₹574.34"}
+        />
+        <Widget
+          icon={<MdDashboard className="h-6 w-6" />}
+          title={"Adjusted Reviews"}
+          subtitle={apiData.adjRev}
+        />
+        <Widget
+          icon={<MdBarChart className="h-7 w-7" />}
+          title={"Adjusted Rating"}
+          subtitle={apiData.adjRat}
+        />
+        <Widget
+          icon={<IoMdHome className="h-6 w-6" />}
+          title={"Gifting"}
+          subtitle={"₹243.3"}
+        />
+      </div>
+
+      {/* Charts */}
+
+      <div className="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2">
+        <TotalSpent />
+        <WeeklyRevenue />
+      </div>
+
+      {/* Tables & Charts */}
+
+      <div className="mt-5 grid grid-cols-1 gap-5 xl:grid-cols-2">
+        {/* Check Table */}
+        <div>
+          <CheckTable
+            columnsData={columnsDataCheck}
+            tableData={tableDataCheck}
+          />
+        </div>
+
+        {/* Traffic chart & Pie Chart */}
+
+        <div className="grid grid-cols-1 gap-5 rounded-[20px] md:grid-cols-2">
+          <PieChartCard title="Fake" cat1 = "Fake" cat2 = "Authentic" sus= {100 - parseInt(Number(apiData.adjRev.replace(",", "")) / Number(apiData.totRev.replace(",", "")) * 100)}/>
+          {/* <PieChartCard title="Fake" sus= {parseInt(Number(apiData.adjRat) / Number(apiData.totRev) * 100)}/> */}
+          <PieChartCard title="Suspicious" cat1 = "Suspicious" cat2 = "Normal" sus= {Number(apiData.sus.perc.substring(1, 3))}/>
+        </div>
+
+        {/* Complex Table , Task & Calendar */}
+
+        <ComplexTable
+          columnsData={complexReview}
+          tableData={tableDataComplex}
+        />
+
+        {/* Task chart & Calendar */}
+
+        <div className="grid grid-cols-1 gap-5 rounded-[20px] md:grid-cols-2">
+          {/* <TaskCard /> */}
+          <DailyTraffic />
+          <div className="grid grid-cols-1 rounded-[20px]">
+            <MiniCalendar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
